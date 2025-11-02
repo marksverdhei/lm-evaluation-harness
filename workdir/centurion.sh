ea67eedb0540a79b59931e47e5f2bdb44c95caf4
marksverdhei <marksverdhei@hotmail.com>
2025-11-02 11:00:39 +0100

    update centurion.sh

---
+++ workdir/centurion.sh
@@ -0,0 +1,13 @@
+alias lm_eval=/home/me/Repos/lm-evaluation-harness/.venv/bin/lm_eval
+
+lm_eval \
+  --model local-chat-completions \
+  --tasks nortruthfulqa_gen_nob_p5 \
+  --model_args base_url=http://localhost:8000/v1/chat/completions,num_concurrent=20,max_retries=999,tokenized_requests=False,model="Gemma-nano-vllm" \
+  --apply_chat_template \
+  --gen_kwargs max_tokens=5000,max_completion_tokens=4096 \
+  --output_path ./results/ \
+  --log_samples \
+  --wandb_args project=lm-eval-harness
+
+# lm_eval --model local-chat-completions --tasks nortruthfulqa_gen_nob_p5 --model_args base_url=http://localhost:8000/v1/chat/completions,num_concurrent=1,max_retries=999,tokenized_requests=False,model="Gemma-nano-llamacpp" --apply_chat_template --gen_kwargs max_tokens=5000,max_completion_tokens=4096 --output_path ./results/ --log_samples --wandb_args project=lm-eval-harness
